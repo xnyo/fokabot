5e8ee034c90c351813753695c9aebc9544f0465e
Giuseppe Guerra <to@nyo.zz.mu>
2019-02-14 00:32:40 +0100

    ASCII art, of course

--- fokabot.py
+++ fokabot.py
@@ -6,7 +6,16 @@ from singletons.bot import Bot
 
 # TODO: uvloop
 logging.basicConfig(level=logging.DEBUG if Config()["DEBUG"] else logging.INFO)
-logging.info("FOKAWOOOOO")
+logging.info(
+    """
+  __      _                                   
+ / _|    | |                                  
+| |_ ___ | | ____ ___      _____   ___   ___  
+|  _/ _ \| |/ / _` \ \ /\ / / _ \ / _ \ / _ \ 
+| || (_) |   < (_| |\ V  V / (_) | (_) | (_) |
+|_| \___/|_|\_\__,_| \_/\_/ \___/ \___/ \___/ 
+"""
+)
 Bot(
     host=Config()["IRC_HOST"],
     port=Config()["IRC_PORT"],
