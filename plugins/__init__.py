5f6f62a1f6e35a1f1c26d2327feb78d938d69e10
Giuseppe Guerra <to@nyo.zz.mu>
2019-02-13 23:52:55 +0100

    Parsing and decorator improvements, add !help command

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,30 +1,55 @@
 from itertools import zip_longest
-from typing import Callable, Any, Tuple
+from typing import Callable, Any, Tuple, Optional, Iterable, Sequence
 
-from schema import SchemaError
-from collections import namedtuple
+from schema import SchemaError, Schema, Or
 
-Arg = namedtuple("Arg", ("key", "schema", "default"), defaults=(None,) * 3)
+
+class Arg:
+    def __init__(self, key: Optional[str] = None, schema: Optional[Schema] = None, default: Optional[Any] = None, rest: bool = False):
+        self.key = key
+        self.schema = schema
+        self.default = default
+        self.rest = rest
+
+    def __str__(self):
+        return f"{self.key}{f'={self.default}' if self.default is not None else ''}{'...' if self.rest else ''}"
+
+    @property
+    def optional(self):
+        return self.default is not None
+
+
+class BotSyntaxError(Exception):
+    def __init__(self, args):
+        self.args = args
 
 
 def arguments(*args: Tuple[Arg]) -> Callable:
+    # TODO: Check optional args only at the end
+    for x in args:
+        if x.optional:
+            x.schema = Or(x.schema, None)
+
     def decorator(f: Callable) -> Callable:
         async def wrapper(username: str, channel: str, message: str, *_, **__) -> Any:
             parts = message.split(" ")[1:]
             validated_args = {}
+            if args[-1].rest:
+                parts = [x for x in parts[:len(args) - 1]] + ([" ".join(parts[len(args) - 1:])] if parts[len(args) - 1:] else [])
+            print(parts)
             for arg, part in zip_longest(args, parts, fillvalue=None):
                 try:
                     v = arg.schema.validate(part)
                     if v is None:
-                        if arg.optional:
-                            v = arg.default
+                        raise ValueError()
                     validated_args[arg.key] = v
                 except (SchemaError, ValueError) as e:
-                    if arg.default is not None:
+                    if arg.optional:
                         validated_args[arg.key] = arg.default
                     else:
-                        print(str(e))
-                        return
+                        raise BotSyntaxError(args)
+                    # print(e)
+                    # raise BotSyntaxError(args)
             return await f(username, channel, validated_args)
         return wrapper
     return decorator
@@ -44,3 +69,17 @@ def private_only(f: Callable) -> Callable:
 
 def public_only(f: Callable) -> Callable:
     return _private_public_wrapper(False, f)
+
+
+def errors(f: Callable) -> Callable:
+    async def wrapper(username: str, channel: str, message: str, *args, **kwargs) -> Any:
+        try:
+            return await f(username, channel, message, *args, **kwargs)
+        except BotSyntaxError as e:
+            first_optional = next((x for x in e.args if x.optional), None)
+            return f"Syntax: {message.split(' ')[0]} {' '.join(str(x) if first_optional is None or x != first_optional else f'[{str(x)}' for x in e.args)}{']' if first_optional is not None else ''}"
+    return wrapper
+
+
+def base(f: Callable) -> Callable:
+    return errors(f)
--- plugins/general.py
+++ plugins/general.py
@@ -1,18 +1,46 @@
-from typing import Dict, Any
+from typing import Dict
 import random
+from aiotinydb import AIOTinyDB
 
-from schema import Use, And
+from schema import Use, And, Schema
 
-from plugins import arguments, Arg, public_only
+from plugins import arguments, Arg, errors, base
 from singletons.bot import Bot
 
 bot = Bot()
 
 
 @bot.command("roll")
+@base
 @arguments(
     Arg("number", And(Use(int), lambda x: x > 0), default=100)
 )
-@public_only
-async def roll(username: str, channel: str, params: Dict[str, Any]):
+async def roll(username: str, channel: str, params: Dict[str, int]):
     return f"{username} rolls {random.randrange(0, params['number'])} points!"
+
+
+@bot.command("help")
+@base
+async def help_(*_, **__):
+    return "Click (here)[https://ripple.moe/index.php?p=16&id=4] for FokaBot's full command list"
+
+
+@bot.command("faq")
+@base
+@arguments(
+    Arg("topic", Schema(str))
+)
+async def faq(username: str, channel: str, params: Dict[str, str]):
+    async with AIOTinyDB(".db.json") as db:
+        print(db.table("faq").all())
+
+
+@bot.command("modfaq")
+@base
+@arguments(
+    Arg("topic", Schema(str)),
+    Arg("response", Schema(str), rest=True),
+)
+async def mod_faq(username: str, channel: str, params: Dict[str, str]):
+    async with AIOTinyDB(".db.json") as db:
+        print(db.table("faq").all())
