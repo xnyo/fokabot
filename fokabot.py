461af6777e135fd82716fc9fc784087fb3afce30
Giuseppe Guerra <to@nyo.zz.mu>
2019-07-25 22:21:18 +0200

    main(), uvloop is now optional

--- fokabot.py
+++ fokabot.py
@@ -1,64 +1,74 @@
 import importlib
 import logging
 
-import uvloop
+try:
+    import uvloop
+    use_uvloop = True
+except ImportError:
+    use_uvloop = False
 
 from singletons.config import Config
 from singletons.bot import Bot
-
-
-# Logging
 from utils.letsapi import LetsApiClient
 from utils.rippleapi import BanchoApiClient, RippleApiClient, CheesegullApiClient
 
-logging.basicConfig(level=logging.DEBUG if Config()["DEBUG"] else logging.INFO)
-logging.info(
+
+def main() -> None:
+    # Logging
+    logging.basicConfig(level=logging.DEBUG if Config()["DEBUG"] else logging.INFO)
+    logging.info(
+        """
+      __      _                                   
+     / _|    | |                                  
+    | |_ ___ | | ____ ___      _____   ___   ___  
+    |  _/ _ \\| |/ / _` \\ \\ /\\ / / _ \\ / _ \\ / _ \\ 
+    | || (_) |   < (_| |\\ V  V / (_) | (_) | (_) |
+    |_| \\___/|_|\\_\\__,_| \\_/\\_/ \\___/ \\___/ \\___/ 
     """
-  __      _                                   
- / _|    | |                                  
-| |_ ___ | | ____ ___      _____   ___   ___  
-|  _/ _ \| |/ / _` \ \ /\ / / _ \ / _ \ / _ \ 
-| || (_) |   < (_| |\ V  V / (_) | (_) | (_) |
-|_| \___/|_|\_\__,_| \_/\_/ \___/ \___/ \___/ 
-"""
-)
+    )
 
-# Setup Bot singleton
-uvloop.install()
-Bot(
-    wss=Config()["WSS"],
-    nickname=Config()["BOT_NICKNAME"],
-    commands_prefix=Config()["COMMANDS_PREFIX"],
-    bancho_api_client=BanchoApiClient(
-        Config()["BANCHO_API_TOKEN"],
-        Config()["BANCHO_API_BASE"]
-    ),
-    ripple_api_client=RippleApiClient(
-        Config()["RIPPLE_API_TOKEN"],
-        Config()["RIPPLE_API_BASE"]
-    ),
-    lets_api_client=LetsApiClient(
-        Config()["LETS_API_BASE"]
-    ),
-    cheesegull_api_client=CheesegullApiClient(
-        Config()["CHEESEGULL_API_BASE"]
-    ),
-    http_host=Config()["HTTP_HOST"],
-    http_port=Config()["HTTP_PORT"],
-    redis_host=Config()["REDIS_HOST"],
-    redis_port=Config()["REDIS_PORT"],
-    redis_database=Config()["REDIS_DATABASE"],
-    redis_password=Config()["REDIS_PASSWORD"],
-    redis_pool_size=Config()["REDIS_POOL_SIZE"]
-)
-# Register all events
-import events
+    # Setup Bot singleton
+    if use_uvloop:
+        uvloop.install()
+        logging.info("Using uvloop")
+    else:
+        logging.warning("Using asyncio")
+    Bot(
+        wss=Config()["WSS"],
+        nickname=Config()["BOT_NICKNAME"],
+        commands_prefix=Config()["COMMANDS_PREFIX"],
+        bancho_api_client=BanchoApiClient(
+            Config()["BANCHO_API_TOKEN"],
+            Config()["BANCHO_API_BASE"]
+        ),
+        ripple_api_client=RippleApiClient(
+            Config()["RIPPLE_API_TOKEN"],
+            Config()["RIPPLE_API_BASE"]
+        ),
+        lets_api_client=LetsApiClient(
+            Config()["LETS_API_BASE"]
+        ),
+        cheesegull_api_client=CheesegullApiClient(
+            Config()["CHEESEGULL_API_BASE"]
+        ),
+        http_host=Config()["HTTP_HOST"],
+        http_port=Config()["HTTP_PORT"],
+        redis_host=Config()["REDIS_HOST"],
+        redis_port=Config()["REDIS_PORT"],
+        redis_database=Config()["REDIS_DATABASE"],
+        redis_password=Config()["REDIS_PASSWORD"],
+        redis_pool_size=Config()["REDIS_POOL_SIZE"]
+    )
+    # Register all events
+    import events
 
-# Import all required plugins (register bot commands)
-for plugin in Config()["BOT_PLUGINS"]:
-    importlib.import_module(f"plugins.{plugin}")
-    Bot().logger.info(f"Loaded plugin plugins.{plugin}")
+    # Import all required plugins (register bot commands)
+    for plugin in Config()["BOT_PLUGINS"]:
+        importlib.import_module(f"plugins.{plugin}")
+        Bot().logger.info(f"Loaded plugin plugins.{plugin}")
 
-# Finally, run the bot
-Bot().run()
-# todo: handle shutdown
+    # Finally, run the bot
+    Bot().run()
+
+if __name__ == '__main__':
+    main()
