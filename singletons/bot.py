5d2103477b8b2d2f16ae374dddb8ce93889b72cf
Giuseppe Guerra <to@nyo.zz.mu>
2019-11-16 17:44:40 +0100

    🥭 2.2.1 🥭

--- singletons/bot.py
+++ singletons/bot.py
@@ -34,7 +34,7 @@ from constants.api_privileges import APIPrivileges
 
 @singleton.singleton
 class Bot:
-    VERSION: str = "2.2.0"
+    VERSION: str = "2.2.1"
 
     def __init__(
         self, *, nickname: str = "FokaBot", wss: bool = True,
